4d7194d2ec01148bc7adbd455ec462e4b0fd9e94
Camila Vivo <47572585@est.ort.edu.ar>
2024-06-18 17:19:51 -0300

    Created_GettingStartedProject

--- Getting_BPM_to_Monitor/Getting_BPM_to_Monitor/Getting_BPM_to_Monitor.ino
+++ Getting_BPM_to_Monitor/Getting_BPM_to_Monitor/Getting_BPM_to_Monitor.ino
@@ -13,9 +13,9 @@
 #include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
 
 //  Variables
-const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int PulseWire = A0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
 const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
-int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+int Threshold = 200;           // Determine which Signal to "count as a beat" and which to ignore.
                                // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
                                // Otherwise leave the default "550" value. 
                                
