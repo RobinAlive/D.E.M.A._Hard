03f1fc961e4c5793530a48ea44e617a060003555
Camila Vivo <47572585@est.ort.edu.ar>
2024-07-02 12:01:20 -0300

    Create Sensor_DTH11.ino

---
+++ Sensor_DTH11/Sensor_DTH11.ino
@@ -0,0 +1,24 @@
+#include <DHT.h>
+
+#define DHTTYPE DHT11
+#define DHTPIN 9
+
+DHT dht (DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay (2000);
+  float temperature = dht.readTemperature();
+
+  if (isnan(temperature)){
+    Serial.println("ERROR EN EL SENSOR");
+  }
+
+  Serial.print ("Temperatura:");
+  Serial.print (temperature);
+  Serial.print ("°C");
+}
